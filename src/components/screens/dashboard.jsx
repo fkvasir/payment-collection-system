d49d428e132a4241abd7ffbd6107827354f85428
Fulgent Kvasir Lavesores <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2024-11-27 08:47:24 +0800

    feat: dashboard

---
+++ src/components/screens/dashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div className="flex flex-col h-screen p-6 bg-gray-50 overflow-hidden">
+      <article className="text-xl font-bold mb-4">Dashboard</article>
+    </div>
+  );
+}
